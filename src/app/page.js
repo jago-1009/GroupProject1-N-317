d8eefc951576136bc4e45e2b6689c77e1f67db92
fralcala <fralcala@iu.edu>
2024-10-19 17:38:14 -0400

    Added nav and started home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className={styles.mainHome}>
+      <h1>Armadillos</h1>
+    </main>
+  );
 }
