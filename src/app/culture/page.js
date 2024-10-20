c90b37d8863f23a9069241936caf1e5a0742e03e
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-10-19 20:53:36 -0400

    Added Culture Header, Content, and InfoCard Component
    Added Culture Header, Culture Content, and Component for InfoCard to be used later.

--- src/app/culture/page.js
+++ src/app/culture/page.js
@@ -1,3 +1,16 @@
+import cultureStyles from "./culture.module.css";
+import InfoCard from "@/components/infoCard";
 export default function Culture() {
-  return <main></main>;
+  return <div className={cultureStyles.mainCulture}>
+    <div className={cultureStyles.container}>
+  <h1 className={cultureStyles.title}>Culture</h1>
+  <div className={cultureStyles.content}>
+  <p>Armadillos have a presence in cultures all over the world! They have a place in many indigenous cultures' stories and tales. Click on the cards below to learn more about Armadillos around the world!</p>
+  <div className={cultureStyles.infoCards}>
+  <InfoCard title="Mexico" content="In indigenous South American cultures around Mexico, the Armadillo is prominent throughout many stories, signifying bountiful crops and a good harvest."/>
+  <InfoCard title="Argentina" content="In Argentinian cultures, the Armadillo is represented as clever and quick-witted, and it is found in many stories and tales, including 'The Armadillo and the Fox'." />
+  </div>
+  </div>
+  </div>
+  </div>;
 }
---
+++ src/components/infoCard/index.js
@@ -0,0 +1,18 @@
+import infoCardStyles from "./infoCard.module.css";
+const baseURL = "https://"
+export default function InfoCard({title, content, href}) {
+    return (<div className={infoCardStyles.infoCard}>
+        <a href={baseURL + href} className={infoCardStyles.link}>
+        <h2 className={infoCardStyles.title}>{title}</h2>
+        <p className={infoCardStyles.content}>{content}</p>
+        </a>
+        <svg width="0" height="0" viewBox="0 0 463 558" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <clipPath id="rounded">
+            <path d="M41.57 23.8328C-9.46601 0.544297 8.92375 125.596 8.92375 125.596C8.92375 125.596 -8.15861 209.67 8.92375 245.805C26.0061 281.94 202.868 337.392 184.48 266.94C166.093 196.488 224.32 103.14 190.609 30.0459C156.899 -43.0477 92.606 47.1213 41.57 23.8328Z" stroke="black"/>
+</clipPath>
+</defs>
+</svg>
+
+
+    </div> )}
