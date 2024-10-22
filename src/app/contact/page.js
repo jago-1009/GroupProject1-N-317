d51d7d8c264c26986e4b5990d5e00ceca8244e4c
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-10-22 15:19:11 -0400

    Contact
    Contact page is done.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,17 @@
 import styles from "./contact.module.css";
 
 export default function Contact() {
-  return <main></main>;
+  return (
+    <main className={styles.contactMain}>
+      <h1>Contact Us!</h1>
+      <input placeholder="Name"></input>
+      <input placeholder="Email" type="email"></input>
+      <textarea placeholder="Message"></textarea>
+      <input
+        type="Submit"
+        placeholder="Submit"
+        className={styles.submit}
+      ></input>
+    </main>
+  );
 }
